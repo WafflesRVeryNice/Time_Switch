c3201ecb928b880a9f4f1c3e07e442e178798dd3
WafflesAreNice <Lilith.Eisenhardt@gmx.com>
2024-12-01 19:38:39 +0000

    made most of teleportation functionality but missing last wee bit (non-functional)

--- Source/Time_SwitchModule.cs
+++ Source/Time_SwitchModule.cs
@@ -4,6 +4,9 @@ using Celeste;
 using On.Celeste;
 using On.Celeste.Mod;
 using IL.Celeste;
+using static MonoMod.InlineRT.MonoModRule;
+using Microsoft.Xna.Framework;
+using Monocle;
 
 namespace Celeste.Mod.Time_Switch;
 
@@ -18,6 +21,29 @@ public class Time_SwitchModule : EverestModule {
 
     public override Type SaveDataType => typeof(Time_SwitchModuleSaveData);
     public static Time_SwitchModuleSaveData SaveData => (Time_SwitchModuleSaveData) Instance._SaveData;
+
+    //vars
+    private Vector2 playerPosition;
+
+    public Scene Scene { get; private set; }
+
+    private Vector2 currentLevelPos;
+
+    private Vector2 playerPosRelativeToLevel;
+
+    private string currentLevelName;
+
+    private string tpToLevel;
+
+    private int currentLevelNumber;
+
+    private string currentLevelNumberString;
+
+    private string nextLevelName;
+
+    private Vector2 nextLevelPos;
+
+    private Vector2 nextPlayerPosRelativeToRoom;
 
 
     public Time_SwitchModule() {
@@ -36,22 +62,78 @@ public class Time_SwitchModule : EverestModule {
         On.Celeste.Player.Update += Player_Update;
     }
 
+    public override void Unload() 
+    {
+        On.Celeste.Player.Update -= Player_Update;
+    }
+
+
     private void Player_Update(On.Celeste.Player.orig_Update orig, Player self)
     {
         orig(self);
 
         if (self.InControl && Time_SwitchModule.Settings.TimeSwitchBind)
         {
-            Logger.Log(LogLevel.Info, "Waffles - TimeSwitch", "jumped");
+            Logger.Log(LogLevel.Info, "Waffles - TimeSwitch", "teleported");
+
+            TeleportPlayer(self);
         }
         else
         {
-            Logger.Log(LogLevel.Info, "Waffles - TimeSwitch", "did not jump");
+            Logger.Log(LogLevel.Info, "Waffles - TimeSwitch", "did not teleport");
         }
     }
 
-    public override void Unload() 
+    void TeleportPlayer(Player self)
     {
-        On.Celeste.Player.Update -= Player_Update;
+        Level level = (Level)self.Scene;
+
+        Player player = level.Tracker.GetEntity<Player>();
+        playerPosition = player.Position;
+
+
+        currentLevelPos = level.LevelOffset;
+
+        playerPosRelativeToLevel = new Vector2(playerPosition.X - currentLevelPos.X, playerPosition.Y - currentLevelPos.Y);
+
+
+        currentLevelName = level.Session.Level;
+
+        if (currentLevelName.StartsWith("a"))
+        {
+            tpToLevel = "b";
+        }
+        else if (currentLevelName.StartsWith("b"))
+        {
+            tpToLevel = "a";
+        }
+
+        currentLevelNumber = currentLevelName[1] + currentLevelName[2];
+        currentLevelNumberString = currentLevelNumber.ToString();
+
+
+        MapData currentMapData = level.Session.MapData;
+
+        //LevelData nextLevelFirstChar = currentMapData.Levels.Find(item => item.Name[0] == tpToLevel[0]);
+
+        for (int i = 0; i < currentMapData.LevelCount; i++) 
+        {
+            LevelData nextPotentiallevel = currentMapData.Levels.Find(item => item.Name[0] == tpToLevel[0]);
+
+            if (nextPotentiallevel.Name.Contains(currentLevelNumberString))
+            {
+                nextLevelName = nextPotentiallevel.Name;
+
+                nextLevelPos = nextPotentiallevel.LevelOffset;
+            }
+        }
+
+        //nextLevelPos = ;
+
+        nextPlayerPosRelativeToRoom = new Vector2(nextLevelPos.X + playerPosition.X, nextLevelPos.Y + playerPosition.Y);
+
+        //Level.TeleportTo(self, nextLevelName, Player.IntroTypes.None, new Vector2(playerPosRelativeToRoom.X, playerPosRelativeToRoom.Y + 800));
+        //Level.TeleportTo(self, nextLevelName, Player.IntroTypes.None, );
+
     }
 }
